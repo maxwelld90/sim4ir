33910161a6c269aedb15ad6b8a35ac626391f58f
David Maxwell <maxwelld90@gmail.com>
2021-07-05 13:02:39 +0100

    Updating JS

--- docs/assets/js/schedule.js
+++ docs/assets/js/schedule.js
@@ -7,25 +7,29 @@ window.addEventListener('load', function() {
 function convertTimesToLocal() {
     let statusElement = document.querySelector('#timezone-status');
     let toConvert = document.querySelectorAll('.time');
-    let offset = getTimezoneOffset();
     let timezoneString = getTimezoneString();
 
     for (let timeElement of toConvert) {
-        convertTimes(timeElement, offset);
+        convertTimes(timeElement, timezoneString);
     }
     
     statusElement.innerHTML = "<strong>Schedule times advertised below have been converted to your computer's local timezone (<span class='highlighted'>" + timezoneString + "</span>).</strong>";
 }
 
-function convertTimes(element, offset) {
-    console.log(element.innerText);
-    console.log(element.innerHTML);
+function convertTimes(element, timezoneString) {
+    let bothTimes = element.innerText;
+
+    let sampleStr = '2012/04/20 10:10:30 +0200';
+
+    convertTimezone(sampleStr, timezoneString);
+
 }
 
 function getTimezoneString() {
     return Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
-function getTimezoneOffset() {
-    return new Date().getTimezoneOffset();
+/* Function from https://stackoverflow.com/a/54127122 */
+function convertTimezone(date, timezoneString) {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: timezoneString})); 
 }
