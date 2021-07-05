d73137b28a9420b49a5a5bc0e34b87c31997452f
David Maxwell <maxwelld90@gmail.com>
2021-07-05 12:29:09 +0100

    Updated schedule

--- docs/assets/js/schedule.js
+++ docs/assets/js/schedule.js
@@ -7,15 +7,17 @@ window.addEventListener('load', function() {
 function convertTimesToLocal() {
     let statusElement = document.querySelector('#timezone-status');
     let offset = getTimezoneOffset();
+    let timezoneString = getTimezoneString();
 
     console.log(offset);
+    console.log(timezoneString);
 
 
     statusElement.innerHTML = "<strong>Schedule times advertised below have been converted to your computer's local timezone.</strong>";
 }
 
 function getTimezoneString() {
-
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
 function getTimezoneOffset() {
